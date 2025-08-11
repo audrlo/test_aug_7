97c8ae6166bd4dd3f6e9710f5e6b8060cd8d4bf4
audrlo <loaudr@gmail.com>
2025-08-11 16:02:22 -0700

    15 frames instead of 30 frames for computational load

--- test_follow.py
+++ test_follow.py
@@ -42,8 +42,8 @@ class HumanFollowingRobot:
         # RealSense setup
         self.pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15) # 15 frames, maybe could go back to 30
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
         self.pipeline.start(config)
         
         # YOLOv8 setup
